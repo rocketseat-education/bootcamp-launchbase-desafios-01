8053dfe3eec131ba92f860bd71a661f9bcb0fcf8
Bootcamp Maykao <mayk@rocketseat.com.br>
2019-11-19 08:57:15 -0300

    organizando

--- codigos/01-1-aposentadoria.js
+++ codigos/01-1-aposentadoria.js
@@ -1,21 +1,16 @@
 const nome = 'Silvana'
-const sexo = 'M'
+const sexo = 'F'
 const idade = 48
 const contribuicao = 23
 
 const calculoContribuicao = idade + contribuicao
 
-if (sexo == 'M') {
-    if (sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95) {
-        console.log(`${nome}, você pode se aposentar!`)
-    } else { 
-        console.log(`${nome}, você não pode se aposentar!`)
-    }
-  } else {
-    if (contribuicao >= 30 && calculoContribuicao >= 85) {
-        console.log(`${nome}, você pode se aposentar!`)
-    }
-    else {
-        console.log(`${nome}, você não pode se aposentar!`)
-    }
-  }
+// essas variáveis irão retornar true ou false
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+if ( homemPodeAposentar || mulherPodeAposentar) {
+    console.log(`${nome}, você pode se aposentar!`)
+} else {
+    console.log(`${nome}, você não pode se aposentar!`)
+}
